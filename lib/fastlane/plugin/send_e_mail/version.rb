def7574e1ac6305a1b2ce9af6e9585054ef6a9b5
huangj <xyhuangjia@gamil.com>
2018-08-09 15:01:03 +0800

    修改参数

--- lib/fastlane/plugin/send_e_mail/version.rb
+++ lib/fastlane/plugin/send_e_mail/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module SendEMail
-    VERSION = "0.1.2"
+    VERSION = "0.1.1"
   end
 end
--- spec/send_e_mail_action_spec.rb
+++ spec/send_e_mail_action_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane::Actions::SendEMailAction do
   let(:action) { Fastlane::Actions::SendEMailAction }
   describe '#send_e_mail' do
-      params = { "stmpserver" => "smtp.yeah.net", "userName" => "xyhuangjia@yeah.net", "password" => "HJ123456", "recipients" => ["2587171762@qq.com", "1593033769@qq.com"], "subject" => "测试标题", "message_body" => "测试文本" }
+      params = { "stmp_server" => "smtp.yeah.net", "user_name" => "xyhuangjia@yeah.net", "password" => "HJ123456", "recipients" => ["2587171762@qq.com", "1593033769@qq.com"], "subject" => "测试标题", "message_body" => "测试文本" }
     it 'send_e_mail_text' do
         # puts(params)
         # # puts params["stmpserver"]
