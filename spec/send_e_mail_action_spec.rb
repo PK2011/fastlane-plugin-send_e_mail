776b5f8a7ba8a252120e2d63ab8caa457f23e8f5
huangj <xyhuangjia@gamil.com>
2018-08-09 13:54:51 +0800

    xinzen

--- spec/send_e_mail_action_spec.rb
+++ spec/send_e_mail_action_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane::Actions::SendEMailAction do
   let(:action) { Fastlane::Actions::SendEMailAction }
   describe '#send_e_mail' do
-      params = { "stmpserver" => "smtp.yeah.net", "userName" => "xyhuangjia@yeah.net", "password" => "HJ123456", "recipients" => ["2587171762@qq.com", "2587171762@qq.com"], "subject" => "测试标题", "message_body" => "测试文本" }
+      params = { "stmpserver" => "smtp.yeah.net", "userName" => "xyhuangjia@yeah.net", "password" => "HJ123456", "recipients" => ["2587171762@qq.com", "1593033769@qq.com"], "subject" => "测试标题", "message_body" => "测试文本" }
     it 'send_e_mail_text' do
         # puts(params)
         # # puts params["stmpserver"]
